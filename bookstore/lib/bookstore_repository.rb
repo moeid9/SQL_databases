f2fb568fa6fdd2ac086f7cb75b8bef71f8500e04
Moeid Haider <moeidhaider9@gmail.com>
2023-02-01 12:55:06 +0000

    databases ex2

---
+++ bookstore/lib/bookstore_repository.rb
@@ -0,0 +1,38 @@
+#Repository Class
+require_relative 'bookstore'
+class BookStoreRepository
+	
+	def all
+		#prints all records
+		sql = 'SELECT * FROM books'
+		result_set = DatabaseConnection.exec_params(sql,[])
+		books = []
+
+		result_set.each do |record|
+      book = BookStore.new
+      book.id = record['id']
+      book.title = record['title']
+      book.author = record['author_name']
+      
+      books << book
+    end
+		return books
+		
+	end
+	def format_output(books)
+		#prints all records formatted
+		##got this error and am stuck with what to do
+			# undefined method `format_output' for #<BookStore:0x00007f8c008d9a58 @id="1", @title="Nineteen Eighty-Four", @author="George Orwell"> (NoMethodError)
+
+	end
+end
+
+
+## test examples
+# repo = BookStoreRepository.new
+# books = repo.all
+# books.length = 5
+# books.first.id = '1'
+# books.first.name = 'Nineteen Eighty-Four'
+# books[1].format = '1 - Nineteen Eighty-Four - George Orwell'
+##
