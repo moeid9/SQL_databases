67bdb8cfa8263cd927ee748fa5958cc4f6718f5d
Moeid Haider <moeidhaider9@gmail.com>
2023-02-03 10:53:22 +0000

    Created and Test drove the artists repository class

--- music_library/spec/music_library_repository_spec.rb
+++ music_library/spec/music_library_repository_spec.rb
@@ -2,7 +2,7 @@ require 'music_library_repository'
 describe AlbumRepository do
 
   def reset_albums_table
-    seed_sql = File.read('spec/seeds_artists.sql')
+    seed_sql = File.read('spec/seeds_albums.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
     connection.exec(seed_sql)
   end
--- music_library/spec/seeds_artists.sql
+++ music_library/spec/seeds_artists.sql
@@ -1,20 +1,4 @@
--- EXAMPLE
--- (file: spec/seeds_{table_name}.sql)
-
--- Write your SQL seed here. 
-
--- First, you'd need to truncate the table - this is so our table is emptied between each test run,
--- so we can start with a fresh state.
--- (RESTART IDENTITY resets the primary key)
-
 TRUNCATE TABLE artists RESTART IDENTITY;
-TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
-
--- Below this line there should only be `INSERT` statements.
--- Replace these statements with your own seed data.
 
 INSERT INTO artists (name, genre) VALUES ('Drake', 'Rap');
-
-
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Her Loss', '2022', '1');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Honestly, Never Mind', '2022', '1');
+INSERT INTO artists (name, genre) VALUES ('BTS', 'Kpop');
